05dd0ce2562c2abfa414e91a1a92c3369f4ffd04
EarlySummerRain <cinense38@gmail.com>
2023-09-14 06:36:30 +0800

    Changed Images to Text buttons

--- main.py
+++ main.py
@@ -30,12 +30,43 @@ root.title("Eight-Puzzle Game")
 root.geometry('330x350')
 root.resizable(0,0) 
 
+#colors
+color1 = '#020f12'
+color2 = '#05d7ff'
+color3 = '#65e7ff'
+color4 = 'BLACK'
+
+#create main frame
+mainFrame = tk.Frame(root, bg=color1)
+mainFrame.pack(fill=tk.BOTH, expand=True)
+mainFrame.columnconfigure(0, weight=1)
+mainFrame.columnconfigure(1, weight=1)
+mainFrame.columnconfigure(2, weight=1)
+mainFrame.rowconfigure(0, weight=1)
+mainFrame.rowconfigure(1, weight=1)
+mainFrame.rowconfigure(2, weight=1)
+
 #checks is solvable
 is_solvable = False
 if is_solvable:
-    solvability_label = tk.Label(root, text="Puzzle is solvable")
+    solvtext = ""
+    solvability_label = tk.Label(
+        mainFrame, 
+        text="Puzzle is solvable",
+        relief="flat",
+        background=color1,
+        fg="WHITE",
+        font=('Arial',10,'bold')
+        )
 else:
-    solvability_label = tk.Label(root, text="Puzzle is not solvable")
+    solvability_label = tk.Label(
+        mainFrame, 
+        text="Puzzle is not solvable",
+        relief="flat",
+        fg="WHITE",
+        background=color1,
+        font=('Arial',10,'bold')
+        )
 solvability_label.grid(row=4, columnspan=3)
 
 #create grid
@@ -44,11 +75,24 @@ puzzle_board = [[None, None, None], [None, None, None], [None, None, None]]
 for i in range(3):
     for j in range(3):
         if initial[i][j] != '0':
-            img = tk.PhotoImage(file="./images/" + initial[i][j] +".png")
-            puzzle_board[i][j] = tk.Button(root, image=img, command=update_puzzle_board, relief="solid")
-            puzzle_board[i][j].image = img
+            puzzle_board[i][j] = tk.Button(
+                mainFrame, 
+                text = initial[i][j], 
+                command=update_puzzle_board, 
+                relief="solid",
+                background=color2,
+                foreground=color4,
+                activebackground=color3,
+                highlightthickness=2,
+                highlightcolor='WHITE',
+                height=10,
+                width=10,
+                border=3,
+                font=('Arial',30,'bold')
+                )
             puzzle_board[i][j].number = initial[i][j]
-            puzzle_board[i][j]. = 
+            puzzle_board[i][j].x = i
+            puzzle_board[i][j].j = i
             puzzle_board[i][j].grid(row=i, column=j, pady = 2, padx = 2)
         else:
             empty_grid["x"] = i
